2d988fe8d598c7d6b9ede260638fc3db52ec25af
Sydney James <sydneyjames7@gmail.com>
2020-12-15 20:45:38 -0800

    failing test for GET all logs

--- __tests__/log.test.js
+++ __tests__/log.test.js
@@ -32,6 +32,7 @@ describe('log routes', () => {
     return request(app)
       .post('/api/v1/logs')
       .send({
+        recipeId: recipe.id,
         dateOfEvent: '2021-01-01',
         notes: 'party',
         rating: 80
@@ -39,10 +40,35 @@ describe('log routes', () => {
       .then(res => {
         expect(res.body).toEqual({
           id: expect.any(String),
-          recipeId: null,
+          recipeId: recipe.id,
           dateOfEvent: '2021-01-01',
           notes: 'party',
           rating: 80
+        });
+      });
+  });
+
+  it('gets all logs via GET', async() => {
+    const logs = await Promise.all([
+      {
+        recipeId: recipe.id,
+        dateOfEvent: '2020-12-29',
+        notes: 'brooke birthday',
+        rating: 23
+      },
+      {
+        recipeId: recipe.id,
+        dateOfEvent: '2021-02-27',
+        notes: 'margot birthday',
+        rating: 28
+      },
+    ].map(log => Log.insert(log)));
+
+    return request(app)
+      .get('/api/v1/logs')
+      .then(res => {
+        logs.forEach(log => {
+          expect(res.body).toContainEqual(log);
         });
       });
   });
